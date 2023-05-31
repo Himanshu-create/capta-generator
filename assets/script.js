68b9ba567bc8399137ef7dc0f860074de8f8a3c7
Himanshu <hs092001@gmail.com>
2023-05-31 12:30:34 +0530

    completed

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,35 @@
 // JavaScript source code
+var captcha;
+
+function reloadPage() {
+    generate();
+    document.getElementById("key").innerHTML = "";
+}
+
+function generate() {
+
+    document.getElementById("submit").value = "";
+    captcha = document.getElementById("image");
+    var uniquechar = "";
+
+    const randomchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 5; i++) {
+        uniquechar += randomchar.charAt(Math.random() * randomchar.length)
+    }
+    captcha.innerHTML = uniquechar;
+}
+
+function printmsg() {
+    const usr_input = document.getElementById("submit").value;
+
+    if (usr_input == captcha.innerHTML) {
+        document.getElementById("key").innerHTML = "Matched";
+        document.getElementById("key").style.color = "greenyellow";
+        generate();
+    }
+    else {
+        document.getElementById("key").innerHTML = "not Matched, Try Again!";
+        document.getElementById("key").style.color = "red";
+        //generate();
+    }
+}
